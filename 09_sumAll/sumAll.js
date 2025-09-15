6c30ed066ff726a7d7366cfbfb2079bbd9f74e5c
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-15 22:07:04 +0000

    Update file to pass all tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    const ZERO = 0
+    let output = ""
+    // check if numbers are negative values
+    if (num1 < ZERO || num2 < ZERO) output = "ERROR"
+    // check if numbers are NOT Integers
+    else if (!Number.isInteger(num1) || 
+            !Number.isInteger(num2)) output = "ERROR"
+    // calculate sum for numbers in between values
+    else {
+        // assign min & max values
+        let max = (num2 > num1) ? num2 : num1
+        let min = (num2 == max) ? num1 : num2
+        // calculate sum
+        let sum = ZERO
+        for (let i = min; i <= max; i++) {
+            sum += i
+        }
+        output = sum
+    }
+    return output
 };
 
 // Do not edit below this line
