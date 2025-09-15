72088af73ceb56d2f980b3c7f25ee75289ea43e8
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-15 20:43:33 +0000

    Update file to pass all tests

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...varArgs) {
+    const ONE = 1
+    varArgs.forEach(it => {
+        while (array.includes(it)) {
+            let index = array.indexOf(it)
+            array.splice(index, ONE)
+        }
+    })
+    return array
 };
 
 // Do not edit below this line
