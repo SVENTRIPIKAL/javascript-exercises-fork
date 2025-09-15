fced672c283a6cfcda93bcb0482187a130089076
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-15 19:45:24 +0000

    Update file to pass all tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    const ZERO = 0
+    let output = ""
+    
+    if (num < ZERO) { output += "ERROR" }
+    else {
+        for (let i = ZERO; i < num; i++) { output += string }
+    }
+    
+    return output
 };
 
 // Do not edit below this line
