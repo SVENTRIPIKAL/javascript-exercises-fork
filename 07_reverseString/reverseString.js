c97fb0ce1cb944341e7251dc782fe897bfd360a7
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-15 19:52:37 +0000

    Update file to pass all tests

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = ""
+    const length = string.length - 1
+    
+    for (let i = length; i >= 0; i--) { output += string[i] }
+    
+    return output
 };
 
 // Do not edit below this line
