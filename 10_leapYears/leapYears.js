6b6ffcf713a7f769b8eeb477a4c57061e691efe4
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-15 22:43:34 +0000

    Update file to pass all tests

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const ZERO = 0
+    const FOUR = 4
+    const ONE_HUNDRED = 100
+    const FOUR_HUNDRED = 400
+    
+    let isLeapYear = false
+    
+    if (year % FOUR == ZERO && 
+        year % ONE_HUNDRED != ZERO) { isLeapYear = true }
+    
+    else if (year % FOUR_HUNDRED == ZERO) { isLeapYear = true }
+    
+    return isLeapYear
 };
 
 // Do not edit below this line
