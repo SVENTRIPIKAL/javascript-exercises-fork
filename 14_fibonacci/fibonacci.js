9aceaed5a7ae4e2d2af97339e43713c5b0222715
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-17 22:29:23 +0000

    Update file to pass all tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const ZERO = 0
+const ONE = 1
 
+function calcFib(n) {
+    let seq = [ONE, ONE]
+    let nextInt = seq[ZERO] + seq[ONE]
+    for (let i = seq.length; i < n; i = seq.length) {
+        let prevInt = seq[i-ONE]
+        seq.push(nextInt)
+        nextInt += prevInt
+    }
+    return seq.pop()
+}
+
+const fibonacci = function(n) {
+    if (Number(n) == NaN || n < ZERO) { return "OOPS" }
+    else if (n == ZERO) { return ZERO }
+    else { return calcFib(n) }
 };
 
 // Do not edit below this line
