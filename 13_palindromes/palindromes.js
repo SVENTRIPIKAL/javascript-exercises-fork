fd56c630914a846da381231dbbe82e1b04cd570a
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-17 20:43:20 +0000

    Update file to pass all tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+// extract letters from string
+function extractAndJoinLetters(string) {
+    const regex = /\w+/g;
+    return string.match(regex).join("").toLowerCase()
+};
+// reverse string
+function reverseString(string) {
+    let reversed = ""
+    for (let i = string.length-1; i >= 0; i--) {
+        reversed += string[i]
+    }
+    return reversed
+};
+// returns true if string is a palindrome
+const palindromes = function (string) {
+    let newString = extractAndJoinLetters(string)
+    let reversed = reverseString(newString)
+    return newString == reversed
 };
 
 // Do not edit below this line
