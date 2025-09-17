3a17d51b7883545bc54c7e395c5428b35483fbee
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-17 07:08:54 +0000

    Update file to pass all tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const ONE = 1
+const FIVE = 5
+const NINE = 9
+const THIRTY_TWO = 32
+
+// returns formatted decimals & whole numbers
+function formatted(n) {
+    if (Number.isInteger(n)) {
+        return n
+    } else {
+        return Number.parseFloat(n.toFixed(ONE))
+    }
+}
+
+// c=(350-32)*5/9
+const convertToCelsius = function(n) {
+    return formatted((n-THIRTY_TWO) * FIVE/NINE)
 };
 
-const convertToFahrenheit = function() {
+// f=(0*9/5)+32
+const convertToFahrenheit = function(n) {
+    return formatted((n*NINE/FIVE) + THIRTY_TWO)
 };
 
 // Do not edit below this line
