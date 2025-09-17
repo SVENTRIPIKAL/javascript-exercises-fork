501fca7a0ea34d734a301af2f8791e9b0fafea2f
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-17 07:39:57 +0000

    Update file to pass all tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const ZERO = 0
+const ONE = 1
+
+const add = function(n1, n2) { return n1 + n2 };
+
+const subtract = function(n1, n2) { return n1 - n2 };
+
+const sum = function(array) {
+  let n = ZERO
+  array.forEach( it => {
+    n += it
+  });
+  return n
 };
 
-const subtract = function() {
-	
+const multiply = function(array) {
+  let n = ONE
+  array.forEach( it => {
+    n *= it
+  });
+  return n
 };
 
-const sum = function() {
-	
-};
+const power = function(base, exp) { return base**exp };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(f) {
+  let n = ONE
+	while (f > ZERO) {
+    n *= f
+    f--
+	}
+	return n
 };
 
 // Do not edit below this line
