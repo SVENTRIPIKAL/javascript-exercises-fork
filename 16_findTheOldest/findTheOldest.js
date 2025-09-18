77d1783c50e03bdd26b45846858b2ab2b13d15a6
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-18 00:12:39 +0000

    Update file to pass all tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const ZERO = 0
+const EMPTY = ""
 
+function getPerson(name, age) {
+    return { name: name, age }
+}
+
+const findTheOldest = function(l) {
+    let n = []
+    l.forEach(it => {
+        let age
+        if (it.yearOfDeath == null) {
+            age = new Date().getFullYear() - it.yearOfBirth
+        } else {
+            age = it.yearOfDeath - it.yearOfBirth
+        }
+        n.push(getPerson(it.name, age))
+    })
+    let oldest = getPerson(EMPTY, ZERO)
+    n.forEach(it => {
+        if (it.age > oldest.age) oldest = it
+    })
+    return oldest
 };
 
 // Do not edit below this line
